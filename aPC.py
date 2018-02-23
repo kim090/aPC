c6ef175036063aa07a874a64200a061b5c1ca328
kim090 <36762205+kim090@users.noreply.github.com>
2018-02-23 12:34:50 +0100

    Add files via upload

--- aPC.py
+++ aPC.py
@@ -2,6 +2,7 @@ import itertools
 import numpy as np
 import matplotlib.pyplot as plt
 from testFun import testFun
+from computeSets import allSets
 
 """ 
 Arbitrary Polynomial Chaos: instead of using polynomials that depends on the distributions 
@@ -9,38 +10,6 @@ of respective input variables, we compute the polynomial coefficients from the r
 data that is actually used. Once the PC expansion is done we feed it with more data from 
 the different distributions.
 """
-
-def multiSet(k, M):
-	"""Computes all combinations of M bins containing 
-	number from 0-k keep those who sums upp to k
-
-	Params:
-		k: max order of polynomial
-		M: number of dimensions (bins)
-
-	Return:
-		mSet: list of arrays which sets sums up to order k
-	"""
-	result = itertools.product(range(k+1),repeat=M) # compute all possible combinations of k number in M bins
-	mSet = [np.array(i) for i in result if sum(i)==k] # computes all the combinations where the number in the bins sums up to k
-	return mSet
-
-def allSets(k,M):
-	""" Append all multi sets 
-
-	Params:
-		k: order of polynomial
-		M: number of variables
-
-	Return:
-		aSet: list of arrays containg all multiSets from 0-k
-	"""
-	aSet = multiSet(0,M)
-
-	for i in range(1,k+1):
-		aSet = np.append(aSet, multiSet(i, M), 0)
-
-	return aSet
 
 def rawMoments(R, k):
 	"""Computes 2k raw statistical moments from data R and order k
